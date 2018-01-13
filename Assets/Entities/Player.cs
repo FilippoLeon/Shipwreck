812d1f41d71523b8b39e437bb0e309f22358f394
Filippo Leonardi <filippo.leon@gmail.com>
2018-01-13 21:21:55 +0100

    Refactor

---
+++ Assets/Entities/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    class Player : Entity<Player> {
+        int funds;
+
+        List<Part> inventory = new List<Part>();
+
+        public override Player Clone() {
+            throw new NotImplementedException();
+        }
+}
